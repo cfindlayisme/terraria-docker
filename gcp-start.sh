c7b4592e40d98cbba93e1e658bbfe65c403b2f84
Chuck Findlay <chuck@findlayis.me>
2022-10-19 20:57:01 -0600

    Moved crontab GCP to start script

--- gcp-start.sh
+++ gcp-start.sh
@@ -5,6 +5,10 @@
 # Author: Chuck Findlay <chuck@findlayis.me>
 # License: LGPL v3.0
 
+echo "@hourly gcloud auth activate-service-account --key-file=/config/gcs-key.json && gsutil cp /config/World.wld gs://$GCS_BUCKET$GCS_BUCKET_PATH/World.wld\n" > cron.d/gcs-hourly-backup
+chmod 0644 /etc/cron.d/gcs-hourly-backup
+crontab /etc/cron.d/gcs-hourly-backup
+
 # Start cron so it runs auto backups
 # TODO: pass env variables for GCS account and project to cron somehow? Or does it manage to make it here?
 cron
