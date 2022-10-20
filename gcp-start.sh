c5b883f1f3fbe8a0b45c46c638d99e56b1631f10
Chuck Findlay <chuck@findlayis.me>
2022-10-19 22:23:00 -0600

    Path correction

--- gcp-start.sh
+++ gcp-start.sh
@@ -5,7 +5,7 @@
 # Author: Chuck Findlay <chuck@findlayis.me>
 # License: LGPL v3.0
 
-echo "@hourly gcloud auth activate-service-account --key-file=/config/gcs-key.json && gsutil cp /config/World.wld gs://$GCS_BUCKET$GCS_BUCKET_PATH/World.wld\n" > cron.d/gcs-hourly-backup
+echo "@hourly gcloud auth activate-service-account --key-file=/config/gcs-key.json && gsutil cp /config/World.wld gs://$GCS_BUCKET$GCS_BUCKET_PATH/World.wld\n" > /etc/cron.d/gcs-hourly-backup
 chmod 0644 /etc/cron.d/gcs-hourly-backup
 crontab /etc/cron.d/gcs-hourly-backup
 
