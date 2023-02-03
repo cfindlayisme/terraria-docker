b96a6e4f2bc75cfe32e6fb3948ec71ee6f9e1bae
Chuck Findlay <chuck@findlayis.me>
2023-02-02 21:37:51 -0700

    Adjusted to GCP backup every six hours, instead of hourly

--- gcp-start.sh
+++ gcp-start.sh
@@ -5,7 +5,7 @@
 # Author: Chuck Findlay <chuck@findlayis.me>
 # License: LGPL v3.0
 
-echo "@hourly gcloud auth activate-service-account --key-file=/config/gcs-key.json && gsutil cp /config/World.wld gs://$GCS_BUCKET$GCS_BUCKET_PATH/World.wld" > /etc/cron.d/gcs-hourly-backup
+echo "0 */6 * * * gcloud auth activate-service-account --key-file=/config/gcs-key.json && gsutil cp /config/World.wld gs://$GCS_BUCKET$GCS_BUCKET_PATH/World.wld" > /etc/cron.d/gcs-hourly-backup
 chmod 0644 /etc/cron.d/gcs-hourly-backup
 crontab /etc/cron.d/gcs-hourly-backup
 
